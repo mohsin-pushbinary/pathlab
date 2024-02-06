f41d62dabaf445c4db979d71f9ef7e6495cdc6c9
mohsin-pushbinary <47211596+mohsin-pushbinary@users.noreply.github.com>
2024-02-06 16:15:03 +0530

    address update

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,22 @@
 
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
+// // Add active class to the current button (highlight it)
+// var header = document.getElementById("myDIV");
+// var btns = header.getElementsByClassName("gender");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("genactive");
+//   current[0].className = current[0].className.replace(" genactive", "");
+//   this.className += " genactive";
+//   });
+// }
+function checkgender(){
+    var header = document.getElementById("myDIV");
 var btns = header.getElementsByClassName("gender");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  var current = document.getElementsByClassName("genactive");
+  current[0].className = current[0].className.replace(" genactive", "");
+  this.className += " genactive";
   });
+}
 }
